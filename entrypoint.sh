4b368cd1a19530ceec3e15d7a3a99fd67fec6c8a
milehimikey <mkey@pax8.com>
2021-02-23 16:26:26 -0700

    export the var for good measure

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,5 @@ fi
 cd ${full_path}
 
 echo "Decrypting ${full_path}/secrets.yaml"
-AWS_SDK_LOAD_CONFIG=1 sops --aws-profile ${secrets_directory} -d "${full_path}/secrets.yaml" > $(basename "${full_path}/secrets.yaml.dec")
+export AWS_SDK_LOAD_CONFIG=1
+sops --aws-profile ${secrets_directory} -d "${full_path}/secrets.yaml" > $(basename "${full_path}/secrets.yaml.dec")
