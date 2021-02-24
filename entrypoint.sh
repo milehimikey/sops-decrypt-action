4a0ce626f9de400e2f854ebb5a0a251f0782b2cc
milehimikey <mkey@pax8.com>
2021-02-24 15:46:36 -0700

    Add AWS specifics˚

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,23 @@ set -euo pipefail
 
 secrets_directory=${1}
 
-if [ -z "${secrets_directory}" ]; then
+if [ -z "${deployment_environment}" ]; then
     echo "Directory is not set or is empty"
     exit 1
 fi
 
-full_path=${GITHUB_WORKSPACE}/helm/environments/${secrets_directory}
+full_path=${GITHUB_WORKSPACE}/helm/environments/${deployment_environment}
 
 if [ ! -d "${full_path}" ]; then
-    echo "Directory ${secrets_directory} does not exist in ${GITHUB_WORKSPACE}"
+    echo "Deployment environment ${deployment_environment} does not exist in ${GITHUB_WORKSPACE}"
     exit 1
 fi
 
 cd ${full_path}
 
 echo "Decrypting ${full_path}/secrets.yaml"
-sops --aws-profile default -d "${full_path}/secrets.yaml" > $(basename "${full_path}/secrets.yaml.dec")
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
+echo "region = ${AWS_DEFAULT_REGION}" >> ~/.aws/credentials
+echo "role_arn = ${AWS_ROLE_NAME}" >> ~/.aws/credentials
+sops --aws-profile ${deployment_environment} -d "${full_path}/secrets.yaml" > $(basename "${full_path}/secrets.yaml.dec")
